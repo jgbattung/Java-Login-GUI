51953f5f3f4c998b4c19f3b9402d43769b5c32c4
Jireh Battung <jirehgbattung@gmail.com>
2023-09-05 17:12:29 +0800

    center elements

--- LoginGUI/src/GUI.java
+++ LoginGUI/src/GUI.java
@@ -1,72 +1,83 @@
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
+import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
-
 public class GUI implements ActionListener {
-	
-	private static JLabel userLabel;
-	private static JTextField userText;
-	private static JLabel passwordLabel;
-	private static JPasswordField passwordText;
-	private static JButton button;
-	private static JLabel success;
+    private static JLabel userLabel;
+    private static JTextField userText;
+    private static JLabel passwordLabel;
+    private static JPasswordField passwordText;
+    private static JButton button;
+    private static JLabel success;
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Login");
+        JPanel panel = new JPanel(new GridBagLayout());
+        frame.setSize(new Dimension(400, 300));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(panel);
 
-		JFrame frame = new JFrame();
-		JPanel panel = new JPanel();
-		frame.setSize(400, 400);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.add(panel);
-		
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.insets = new Insets(5, 5, 5, 5);
 
-		panel.setLayout(null);
-		
-		userLabel = new JLabel("User");
-		userLabel.setBounds(10, 20, 80, 25);
-		panel.add(userLabel);
-		
-		userText = new JTextField(20);
-		userText.setBounds(100, 20, 165, 25);
-		panel.add(userText);
-		
-		passwordLabel = new JLabel("Password");
-		passwordLabel.setBounds(10, 50, 80, 25);
-		panel.add(passwordLabel);
-		
-		passwordText = new JPasswordField();
-		passwordText.setBounds(100, 50, 165, 25);
-		panel.add(passwordText);
-		
-		button = new JButton("Login");
-		button.setBounds(10, 80, 80, 25);
-		button.addActionListener(new GUI());
-		panel.add(button);
-		
-		success = new JLabel("");
-		success.setBounds(10, 110, 300, 25);
-		panel.add(success);
-		
-		
-		frame.setVisible(true); 
-		
-	}
+        // User Label
+        userLabel = new JLabel("User");
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.anchor = GridBagConstraints.CENTER;
+        panel.add(userLabel, constraints);
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		String user = userText.getText();
-		String password = passwordText.getText();
-		
-		if(user.equals("Jireh") && password.equals("pass123")) {
-			success.setText("Login successful!");
-		}
-		else {
-			success.setText("Wrong username or password");
-		};
-		userText.setText("");
-		passwordText.setText("");
-	}
+        // User Text Field
+        userText = new JTextField(20);
+        constraints.gridx = 1;
+        constraints.gridy = 0;
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        panel.add(userText, constraints);
 
+        // Password Label
+        passwordLabel = new JLabel("Password");
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        panel.add(passwordLabel, constraints);
+
+        // Password Text Field
+        passwordText = new JPasswordField();
+        constraints.gridx = 1;
+        constraints.gridy = 1;
+        panel.add(passwordText, constraints);
+
+        // Login Button
+        button = new JButton("Login");
+        constraints.gridx = 0;
+        constraints.gridy = 2;
+        constraints.gridwidth = 2; 
+        constraints.fill = GridBagConstraints.NONE;
+        panel.add(button, constraints);
+
+        button.addActionListener(new GUI());
+
+        // Success Label
+        success = new JLabel("");
+        constraints.gridx = 0;
+        constraints.gridy = 3;
+        constraints.gridwidth = 2;
+        constraints.anchor = GridBagConstraints.CENTER;
+        panel.add(success, constraints);
+
+        frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String user = userText.getText();
+        String password = passwordText.getText();
+
+        if (user.equals("Jireh") && password.equals("pass123")) {
+            success.setText("Login successful!");
+        } else {
+            success.setText("Wrong username or password");
+        }
+        userText.setText("");
+        passwordText.setText("");
+    }
 }
